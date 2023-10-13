99f12ec72815884f698528d27e0e33910b88b699
EmmaDram <111453762+EmmaDram@users.noreply.github.com>
2023-10-12 20:21:02 -0500

    wrote pair class

--- Chapter 15 Activities/FloodFill/src/PairClass.java
+++ Chapter 15 Activities/FloodFill/src/PairClass.java
@@ -1,1 +1,21 @@
+public class PairClass
+  {
+    private int val1;
+    private int val2;
 
+    public PairClass(int num1, int num2)
+    {
+      val1 = num1;
+      val2 = num2;
+    }
+
+    public int row()
+    {
+      return val1;
+    }
+
+    public int column()
+    {
+      return val2;
+    }
+  }
