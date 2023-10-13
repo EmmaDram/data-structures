a3a998a2e60ff1b815b74f3576602a8367004105
EmmaDram <111453762+EmmaDram@users.noreply.github.com>
2023-10-12 23:43:10 -0500

    Update and rename PairClass.java to Pair.java

--- Chapter 15 Activities/FloodFill/src/Pair.java
+++ Chapter 15 Activities/FloodFill/src/Pair.java
@@ -1,9 +1,9 @@
-public class PairClass
+public class Pair
   {
     private int val1;
     private int val2;
 
-    public PairClass(int num1, int num2)
+    public Pair(int num1, int num2)
     {
       val1 = num1;
       val2 = num2;
